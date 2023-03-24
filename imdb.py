4413c23c92f7681163ff12891b4aa2b5a2e8da6f
nivonl <nivonl@gmail.com>
2023-03-25 00:01:54 +0300

    Add imdb script

--- imdb.py
+++ imdb.py
@@ -5,3 +5,7 @@ Created on Fri Mar 24 23:39:53 2023
 @author: User
 """
 
+"""
+This script will iinclude scraping data from imbd or other api in order to track ratings,
+In Order to get time series data. 
+"""
